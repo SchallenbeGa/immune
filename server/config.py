b7b2e61378fdf90dee892078a9cb1dd3e318971e
Jonathan Othenin-Girard <gofortiss@icloud.com>
2022-02-16 22:23:40 +0100

    Telegram early beta

--- server/config.py
+++ server/config.py
@@ -1,24 +1,27 @@
 #BINANCE
-API_KEY = 'QC4Rik9ajA3bqfEv81ano4tiSb7NuLyFMseG2A1JmES9IX8ULUXh3mCsn7TnF42G'
-API_SECRET = 'sZQ99FhBCR2lQfuERmiT7LzbCKfwvBmLQZLm7eN14ipkGh060lbWgFDiB8QoFmm8'
+API_KEY = 'SiBh6S3tO6iC1UIgcbWDWaCHrug3dTthgD2uorBiex78djOEPBrcOGeYbKwbPCSY'
+API_SECRET = 'dvJIkAWzYfBsTlnyoNPiV7qd5RCzxgsAFjGlyFpBcguEDm2lwj8vGIiiZSmbjZ8h'
 #TWEETER
 TWEET=True
 C_KEY = 'B16J2YTwEBIspDHXBBWZO42pC'
 C_SECRET = 'R8J9tUWf0I4V0O4BmggOHQuwvHOgRUF1aYFvTlIc010SnD9fuR'
 A_T = '1265285941624811520-VtPzQN6E3NQsLtnJx91P1GwfYmXPwp'
 A_T_S = 'rWQfTBOjgoH3dopJ0NhBkH053AtX9HvWrOZaXIUorVVwd'
+#TELEGRAM (Default False, to enable this command please execute :
+#.  telegram-send --configure
+TELEGRAM=True
 #VAR
 PAIR = 'xrpusdt'
 PAIR_S = 'usdt'
 PAIR_B = "xrp"
-QUANTITY = '300'
+QUANTITY = '150'
 GRAPH = False
 #ENVIRONMENT
 DEBUG = False
 TESTNET= False
 #FUTURE
 FUTURE = True
-FUTURE_LEVERAGE = '20'
+FUTURE_LEVERAGE = '10'
 FUTURE_COIN = False # wip
 #STRATEGY VAR
 STRATEGY_NAME = "sma" # sma|rsi|orderbook
