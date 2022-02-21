c42e9271ccf69278797576480e6019d3e653ecb2
Gabriel <gabriel.schallenberger@outlook.com>
2022-02-21 16:19:15 +0100

    Create s_arno.py

---
+++ s_arno.py
@@ -0,0 +1,16 @@
+#place short order with 10% margin of actual price
+#place long order with 20% margin less actual price
+long = False
+switch risk:
+  
+  1 : if 3|var_streak day red : short | 3 day green : short too
+  
+  2 ..
+  
+  3 ..
+  
+  if var_streak > sma_signal :
+    
+  set_stop_loss :
+    
+    
